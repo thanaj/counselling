808949227f91b643cae8841b4241c1ca3af3e2f8
iamtomek <12088991+thanaj@users.noreply.github.com>
2018-07-09 21:44:26 +0200

    sticky navbar added

--- scripts/es6/variables.js
+++ scripts/es6/variables.js
@@ -4,4 +4,7 @@ obj.MENU_BTN_SELECTOR = '[data-target="menu-btn"]'
 obj.MENU_LIST_SELECTOR = '[data-target="menu-list"]'
 obj.MENU_ITEM_SELECTOR ='[data-target="menu-item"]'
 obj.NAV_SELECTOR ='[data-target="nav"]'
+obj.WITAJ_SELECTOR = '[data-target-nav="menu-item-witaj"]'
+obj.NOT_WITAJ_ITEM_SELECTOR = '[data-target-nav="menu-item-not-witaj"]'
+obj.HEADER_SELECTOR ='[data-target="header"]'
 export default obj;
